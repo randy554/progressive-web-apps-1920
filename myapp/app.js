91595b9ae00e7c19805cd9ff472579481d7e5554
randy554 <randy554@hotmail.com>
2020-03-13 13:53:58 +0100

    final

--- myapp/app.js
+++ myapp/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+let dataUserAnswers = [];
+let dataStoreAll = [];
 
-// Hierin frontend CSS/JS
+// Map voor frontend CSS/JS
 app.use(express.static('public'));
+
+// Gebruik de bodyParser om Post waardes te pakken
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Template engine: EJS wordt hier aan de Express app gelinkt
 app.set('view engine', 'ejs');
@@ -18,12 +25,70 @@ app.get('/', (req, res) => {
 
 // Questions page
 app.get('/category/:id/questions', (req, res) => {
-    const id =  req.params.id
-    res.render('questions', {
-        vraagId: id
-    });
+
+    const userCategoryChoice = req.params.id;
+    const questionsAmount = 12;
+    const typeAnswer = 'boolean';
+
+
+    //Build API endpoint
+    const apiEndpoint = `https://opentdb.com/api.php?amount=${questionsAmount}&category=${userCategoryChoice}&type=${typeAnswer}`;
+
+    const data = fetch(apiEndpoint)
+        .then(result => result.json())
+        .then(jsonReturnData => {
+
+            storeAll(jsonReturnData);
+
+            res.render('questions', {
+                vraagId: userCategoryChoice,
+                alleData: jsonReturnData
+            });
+
+        });
 });
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/category/:id/results', (req, res) => {
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+    // console.log('Is die van mij', Object.entries(req.body));
+    // console.log('De sleutels', Object.keys(req.body));
+
+    const answers = Object.values(req.body);
+    storeUserAnswers(answers);
+
+
+    console.log('Opgeslagen', answers);
+    console.log('De waardes', Object.values(req.body));
+    console.log('Answers DB', dataUserAnswers);
+    getLocalApiData();
+
+    getUserAnswer();
+    debugger;
+    const data = req.body;
+
+    console.log('hier is je data: ', data);
+    res.render('results', {
+        userAnswers: data
+    });
+
+});
+
+function storeUserAnswers(userAnswers){
+    dataUserAnswers.push(userAnswers);
+}
+
+function storeAll(apiData) {
+    dataStoreAll.push(apiData);
+}
+
+function getUserAnswer(){
+    console.log('Derde antwoord: ', dataUserAnswers[0][7]);
+}
+
+function getLocalApiData(){
+    console.log('Dit is alle lokale Api Data: ', dataStoreAll[0].results);
+    console.log('Dit is lokale vraag Api Data: ', dataStoreAll[0].results[11].question);
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
